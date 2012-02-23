2a491c50064c51e61cc669a33e4623c8e80e6534
Leon Timmermans <fawaka@gmail.com>
2012-02-23 22:27:07 +0100

    Add stub documentation

--- lib/PerlIO/utf8_strict.pm
+++ lib/PerlIO/utf8_strict.pm
@@ -9,3 +9,20 @@ XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 1;
 
 #ABSTRACT: Fast and correct UTF-8 IO
+
+__END__
+
+=head1 SYNOPSIS
+
+ open my $fh, '<:utf8_strict', $filename;
+
+=head1 DESCRIPTION
+
+This module provides a fast and correct UTF-8 PerlIO layer.
+
+=head1 SYNTAX
+
+The general syntax of the layer is in the form:
+
+ :utf8_strict
+
